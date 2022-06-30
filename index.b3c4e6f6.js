dc8f70f1f37dee46a9407178043284c838dae37b
mankovskiy <35701366+mankovskiy@users.noreply.github.com>
2022-06-30 22:28:10 +0000

    Deploying to gh-pages from @ mankovskiy/goit-js-hw-11@2d4aca15aed080a75b19c76ab1898c93c3ab1605 🚀

---
+++ index.b3c4e6f6.js
@@ -0,0 +1,2 @@
+document.querySelector(".search-form");document.querySelector("button").addEventListener("click",(function(e){e.preventDefault()}));
+//# sourceMappingURL=index.b3c4e6f6.js.map
---
+++ index.befb473e.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector(".search-form");document.querySelector("button").addEventListener("click",(function(e){e.preventDefault()}))}();
+//# sourceMappingURL=index.befb473e.js.map
